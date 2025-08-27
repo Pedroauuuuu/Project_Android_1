71969df30d1f635ab9a326a5e0a692dab4680e48
Pedroauuuuu <Langitezarpedro@gmail.com>
2025-08-27 15:01:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.project1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    int count=0;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+
+        }
+    public void showToast(View view) {
+        Toast.makeText( this, "hello", Toast.LENGTH_SHORT).show();
+    }
+    public void countUp(View view) {
+        count++;
+        textView.setText(String.valueOf(count));
+    }
+}
